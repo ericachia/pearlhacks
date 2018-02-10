97b0139441dc6b36652598ef5730cb0a7cb798ef
ericachia <ercachia@gmail.com>
2018-02-10 15:38:03 -0500

    testing angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
